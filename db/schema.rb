1e838ee2fe64ea41dff4f1222e796e5b1d7e6841
alibabr <babar2166@gmail.com>
2019-11-28 00:18:21 +0500

    added device token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_094544) do
+ActiveRecord::Schema.define(version: 2019_11_27_190800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -147,6 +147,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_094544) do
     t.integer "login_count"
     t.datetime "login_time"
     t.string "stripe_cutomer_id"
+    t.string "device_token"
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
