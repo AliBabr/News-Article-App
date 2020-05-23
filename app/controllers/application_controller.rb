9a4cbd01cc53a126ff3e6e20321031d3c28ba6a1
alibabr <babar2166@gmail.com>
2020-05-23 15:54:39 +0500

    fixed header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
 
   def authenticate
-    @user = User.find_by_id(request.headers['X-NEWS-ARTICLE-USER-ID'])
+    @user = User.find_by_id(request.headers["X_NEWS_ARTICLE_USER_ID"])
     if @user.present?
-      if User.validate_token(request.headers['X-NEWS-ARTICLE-USER-ID'], request.headers['Authentication-Token'])
+      if User.validate_token(request.headers["X_NEWS_ARTICLE_USER_ID"], request.headers["Authentication_Token"])
         return true
       else
-        render json: { message: 'Unauthorized!' }, status: 401
+        render json: { message: "Unauthorized!" }, status: 401
       end
     else
-      render json: { message: 'User Not Found!' }, status: 404
+      render json: { message: "User Not Found!" }, status: 404
     end
   end
 end
